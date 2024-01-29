d7bff406bb523fe38da6d5102877023b777b7315
Siyun Lee <siyun1221@gmail.com>
2024-01-29 22:18:30 +0900

    #3.1 Forms and UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { createGlobalStyle } from 'styled-components'
+import reset from 'styled-reset'
+
 import Layout from './components/Layout'
 import Home from './routes/home'
 import Profile from './routes/profile'
 import Login from './routes/login'
 import CreateAccount from './routes/create-account'
-import { createGlobalStyle } from 'styled-components'
-import reset from 'styled-reset'
-import { useEffect, useState } from 'react'
 import LoadingScreen from './components/LoadingScreen'
 import { auth } from './firebase'
+import styled from 'styled-components'
 
 const router = createBrowserRouter([
   {
@@ -35,18 +37,6 @@ const router = createBrowserRouter([
   },
 ])
 
-const GlobalStyle = createGlobalStyle`
-${reset};
-* {
-  box-sizing: border-box;
-}
-body {
-  background-color: black;
-    color: white;
-    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-}
-`
-
 const App = () => {
   const [isLoading, setLoading] = useState(true)
   const init = async () => {
@@ -59,11 +49,29 @@ const App = () => {
   }, [])
 
   return (
-    <>
+    <Wrapper>
       <GlobalStyle />
       {isLoading ? <LoadingScreen /> : <RouterProvider router={router} />}
-    </>
+    </Wrapper>
   )
 }
 
 export default App
+
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+`
+
+const GlobalStyle = createGlobalStyle`
+${reset};
+* {
+  box-sizing: border-box;
+}
+body {
+  background-color: black;
+    color: white;
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
+`
--- src/routes/create-account.tsx
+++ src/routes/create-account.tsx
@@ -1,3 +1,110 @@
-const CreateAccount = () => <h1>create account</h1>;
+import { useState } from 'react'
+import { styled } from 'styled-components'
 
-export default CreateAccount;
+const CreateAccount = () => {
+  const [isLoading, setLoading] = useState(false)
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [error, setError] = useState('')
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { name, value },
+    } = e
+
+    if (name === 'name') return setName(value)
+    if (name === 'email') return setEmail(value)
+    if (name === 'password') return setPassword(value)
+  }
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    try {
+      // create an account
+      // set the name of the user.
+      // redirect to the home page
+    } catch (e) {
+      // setError
+    } finally {
+      setLoading(false)
+    }
+    console.log(name, email, password)
+  }
+
+  return (
+    <Wrapper>
+      <Title>Login to App</Title>
+      <Form onSubmit={onSubmit}>
+        <Input
+          onChange={onChange}
+          name='name'
+          value={name}
+          placeholder='Name'
+          type='text'
+          required
+        />
+        <Input
+          onChange={onChange}
+          name='email'
+          value={email}
+          placeholder='Email'
+          type='email'
+          required
+        />
+        <Input
+          onChange={onChange}
+          name='password'
+          value={password}
+          placeholder='Password'
+          type='password'
+          required
+        />
+        <Input type='submit' value={isLoading ? 'Loading' : 'Create Account'} />
+      </Form>
+      {error !== '' && <Error>{error}</Error>}
+    </Wrapper>
+  )
+}
+
+export default CreateAccount
+
+const Wrapper = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 420px;
+  padding: 50px 0;
+`
+
+const Title = styled.h1`
+  font-size: 42px;
+`
+
+const Form = styled.form`
+  margin-top: 50px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  width: 100%;
+`
+
+const Input = styled.input`
+  padding: 10px 20px;
+  border-radius: 50px;
+  border: none;
+  width: 100%;
+  font-size: 16px;
+  &[type='submit'] {
+    cursor: pointer;
+    &:hover {
+      opacity: 0.8;
+    }
+  }
+`
+
+const Error = styled.span`
+  font-weight: 600;
+  color: tomato;
+`
